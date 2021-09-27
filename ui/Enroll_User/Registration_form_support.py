40d9129fe1dd3b8f4b4d3806dce82bbcd03fc2a6
singh <singhj775@gmail.com>
2021-09-27 17:12:35 +0530

    Fix Radio Button

---
+++ ui/Enroll_User/Registration_form_support.py
@@ -0,0 +1,167 @@
+import logging
+import math
+import os
+import smtplib
+import sys
+import random as random
+from idlelib.idle_test.test_browser import mb
+from tkinter.messagebox import showinfo
+from tkinter import filedialog as fd, messagebox
+from service.config_service import ConfigService
+
+from PIL import ImageTk
+from PIL.Image import Image
+
+logger = logging.getLogger("Registration_form_support")
+
+configService = ConfigService.getInstance()
+
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+
+try:
+    import ttk
+
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+
+    py3 = True
+
+
+def set_Tk_var():
+    global selectedButton
+    selectedButton = tk.IntVar()
+
+
+def otp():
+    global OTPp
+    OTPp = tk.IntVar()
+    # for alpha nuemeric OTP
+    corpus = "0123456789"
+    OTPp = ""
+    size = 6
+    length = len(corpus)
+    for i in range(size):
+        from random import random
+        OTPp += corpus[math.floor(random() * length)]
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login("775singhj@gmail.com", "27April@1993")
+
+    # message to be sent
+    message = OTPp
+
+    # sending the mail
+    s.sendmail("775singhj@gmail.com", "singhj775@outlook.com", message)
+
+    # terminating the session
+    s.quit()
+
+def register():
+    pass
+    # if OTP_P.get() == "":
+    #     mb.showinfo('Information', "Please Enter otp")
+    #     return
+    # if NAME.get() == "" or LNAME.get() == "" or USERID.get() == "" or PASS.get() == "":
+    #     lbl_result.config(text="Please Fill all the Details", fg="red")
+    #     return
+    # else:
+    #     Database()
+    #     cursor.execute("INSERT INTO `user` (fname, lname, userid, password) VALUES(%s, %s, %s, %s)",
+    #                    (str(NAME.get()), str(LNAME.get()), str(USERID.get()), str(PASS.get())))
+    #     conn.commit()
+    #     conn.close()
+    #     tkMessageBox.showinfo("DATA SAVED", "Your Data is Saved Succesfully")
+    #     root.withdraw()
+    #     Home()
+    #     loginform.destroy()
+
+
+def initViews():
+    logger.debug(configService.getConfig("resources.logo_res"))
+    logo_image = Image.open(configService.getConfig("resources.logo_res"))
+    resized_image = ImageTk.PhotoImage(logo_image.resize((100, 100), Image.ANTIALIAS))
+    w.Canvas1.image = resized_image
+    w.Canvas1.create_image(0, 0, image=resized_image, anchor='ne')
+
+
+def init(top, gui, *args, **kwargs):
+    global w, top_level, root
+    w = gui
+    top_level = top
+    root = top
+    initViews()
+
+
+def destroy_window():
+    # Function which closes the window.
+    global top_level
+    top_level.destroy()
+    top_level = None
+
+
+def radio_gender(genderVar):
+    if genderVar == "male":
+        selection = "You selected the option " + str(genderVar)
+        print(selection)
+        return
+    if genderVar == "female":
+        selection = "You selected the option " + str(genderVar)
+        print(selection)
+        return
+    else:
+        print("error")
+def upload():
+    sql1 = tk.StringVar()
+    sql12 = tk.StringVar()
+
+    filetypes = (('image files', '*.jpg'), ('All files', '*.*'))
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected File',
+        message=[filename]
+    )
+
+    try:
+
+        # if filename:
+        #     image_file_name = os.path.basename(filename)
+        #     file_to_copy = filename
+        #     image_selected = True
+    #         cursor = conn.cursor(buffered=True)
+    #         cursor.execute("create database if not exists meena")
+    #         cursor.execute("use meena")
+    #         cursor.execute(
+    #             '''CREATE TABLE IF NOT EXISTS `test` (image LONGBLOB null,bill_number varchar(100) primary key NOT NULL )''')
+    #         with open(file_to_copy, 'rb') as ff:
+    #             bd = ff.read()
+    #         cursor.execute('''insert into test(image,bill_number) values(%s,%s)''', (bd, billl))
+    #         conn.commit()
+    #
+            mb.showinfo("File uploaded succesfully")
+    #
+    #
+    #
+    except IOError as err:
+        image_selected = False
+        mb.showinfo("File Error", err)
+
+
+if __name__ == '__main__':
+    import registraion_form
+
+    registraion_form.vp_start_gui_registration()
--- ui/mainframe_support.py
+++ ui/mainframe_support.py
@@ -3,7 +3,7 @@ import sys
 
 from service.user_service import UserService
 from service.config_service import ConfigService
-
+from ui.Enroll_User import registration_form as registration_form
 try:
     import Tkinter as tk
 except ImportError:
@@ -41,7 +41,6 @@ def initViews():
     window.logoCanvas.create_image(0, 0, image=resized_image, anchor='nw')
     window.orgTitleLabel.configure(text=configService.getConfig("ui.main_ui.title"))
 
-
 def init(top: tk.Tk, gui, *args, **kwargs):
     global window, top_level, root
     window = gui
@@ -50,9 +49,73 @@ def init(top: tk.Tk, gui, *args, **kwargs):
     initViews()
 
 
-def deleteUser():
-    print('mainframe_support.deleteUser')
-    sys.stdout.flush()
+def delete():
+    pass
+def approve():
+    pass
+
+
+def modify_user():
+        global viewform10
+        viewform10 = tk.Toplevel()
+        viewform10.title("MEENA STOCK /View Product")
+        width = 700
+        height = 400
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        x = (screen_width / 2) - (width / 2)
+        y = (screen_height / 2) - (height / 2)
+        viewform10.geometry("%dx%d+%d+%d" % (width, height, x, y))
+        # viewform10.resizable(0, 0)
+        ViewForm10()
+
+def ViewForm10():
+        global treee, SEARCH
+        SEARCH=tk.StringVar()
+        TopViewForm = tk.Frame(viewform10, width=600, bd=1, relief=tk.SOLID)
+        TopViewForm.pack(side=tk.TOP, fill=tk.X)
+        LeftViewForm = tk.Frame(viewform10, width=600)
+        LeftViewForm.pack(side=tk.LEFT, fill=tk.Y)
+        MidViewForm = tk.Frame(viewform10, width=600)
+        MidViewForm.pack(side=tk.RIGHT)
+        lbl_text = tk.Label(TopViewForm, text="View Products", font=('arial', 18), width=600)
+        lbl_text.pack(fill=tk.X)
+        lbl_txtsearch = tk.Label(LeftViewForm, text="Search", font=('arial', 15))
+        lbl_txtsearch.pack(side=tk.TOP, anchor=tk.W)
+        search = tk.Entry(LeftViewForm, textvariable=SEARCH, font=('arial', 15), width=10)
+        search.pack(side=tk.TOP, padx=10, fill=tk.X)
+        btn_search = tk.Button(LeftViewForm, text="Search", command=delete)
+        btn_search.pack(side=tk.TOP, padx=10, pady=10, fill=tk.X)
+        btn_delete = tk.Button(LeftViewForm, text="Delete", command=approve)
+        btn_delete.pack(side=tk.TOP, padx=10, pady=10, fill=tk.X)
+        scrollbarx = tk.Scrollbar(MidViewForm, orient=tk.HORIZONTAL)
+        scrollbary = tk.Scrollbar(MidViewForm, orient=tk.VERTICAL)
+        treee = ttk.Treeview(MidViewForm, columns=("fname", "lname", "userid"), selectmode="extended", height=100,
+                             yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+        scrollbary.config(command=treee.yview)
+        scrollbary.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollbarx.config(command=treee.xview)
+        scrollbarx.pack(side=tk.BOTTOM, fill=tk.X)
+        treee.heading('fname', text="FIRST NAME", anchor=tk.W)
+        treee.heading('userid', text="USER NAME", anchor=tk.W)
+        treee.heading('fname', text="FIRST NAME", anchor=tk.W)
+        treee.column('#0', stretch=tk.NO, minwidth=0, width=0)
+        treee.column('#1', stretch=tk.NO, minwidth=0, width=0)
+        treee.column('#2', stretch=tk.NO, minwidth=0, width=200)
+        treee.column('#3', stretch=tk.NO, minwidth=0, width=120)
+        treee.pack()
+        #DisplayData()
+
+
+# def DisplayData():
+#     Database()
+#     cursor.execute("SELECT * FROM `user`")
+#     fetch = cursor.fetchall()
+#     for data in fetch:
+#         treee.insert('', 'end', values=(data))
+#     cursor.close()
+#     conn.close()
+
 
 
 def enrollAdmin():
@@ -61,8 +124,7 @@ def enrollAdmin():
 
 
 def enrollUser():
-    print('mainframe_support.enrollUser')
-    sys.stdout.flush()
+    registration_form.vp_start_gui_registration()
 
 
 def login(username, password):
@@ -77,6 +139,10 @@ def login(username, password):
 def takeBackup():
     print('mainframe_support.takeBackup')
     sys.stdout.flush()
+
+
+
+
 
 
 def destroy_window():
